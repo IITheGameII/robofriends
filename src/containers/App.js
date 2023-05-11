9cf26cf198d06eb99cbc7926644b9cdc5c1989ac
Dominik <hdominik96@hotmail.com>
2023-05-11 09:55:59 +0200

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import CardList from "../components/CardList";
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from "../components/ErrorBoundry";
+
+function App() {
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+    const [count, setCount] = useState(0);
+
+    //replicated componentDidMount with the empty array
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => { return response.json(); })
+            .then(users => { setRobots(users) })
+    }, [])
+
+    useEffect(() => {
+        console.log(count)
+    }, [count])
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
+    }
+
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+
+    return !robots.length ?
+        <h1>Loading...</h1> :
+        <div className="tc" >
+            <h1 className="f1">RoboFriends</h1>
+            <button onClick={() => setCount(count + 1)}>Click me!</button>
+            <SearchBox searchChange={onSearchChange} />
+            <Scroll>
+                <ErrorBoundry>
+                    <CardList robots={filteredRobots} />
+                </ErrorBoundry>
+            </Scroll>
+        </div>
+}
+
+export default App;
